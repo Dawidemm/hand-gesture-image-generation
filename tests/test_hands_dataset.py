145eacf042c8bd22bb51291d85be655f685d39d2
Dawidemm <dawid.mazur@icloud.com>
2024-03-20 23:14:37 +0100

    test: Add tests for HandsDataset class.

---
+++ tests/test_hands_dataset.py
@@ -0,0 +1,45 @@
+import pytest
+import torch
+from torchvision import transforms
+from src.utils.hands_dataset import HandsDataset
+
+@pytest.fixture
+def dataset():
+    return HandsDataset(root_dir='dataset/archive', transform=None)
+
+def test_flowers_dataset_creation(dataset):
+    assert dataset is not None
+
+def test_flowers_dataset_getitem(dataset):
+    for i in range(len(dataset)):
+        image, label = dataset[i]
+        assert image is not None
+        assert label is not None
+
+def test_flowers_dataset_len(dataset):
+    assert len(dataset) > 0
+
+def test_flowers_dataset_transform():
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((64, 64))
+    ])
+    dataset = HandsDataset(root_dir='dataset/archive', transform=transform)
+
+    for i in range(len(dataset)):
+        image, label = dataset[i]
+        assert image.size() == (3, 64, 64)
+        assert label is not None
+
+def test_flowers_dataset_image_size(dataset):
+    size = (3, 256, 256)
+
+    for i in range(len(dataset)):
+        image, label = dataset[i]
+        assert image.size() == size
+        assert label is not None
+
+def test_flowers_dataset_image_type(dataset):
+    for i in range(len(dataset)):
+        image, _ = dataset[i]
+        assert isinstance(image, torch.Tensor)
