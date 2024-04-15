4e4b2c518e898f7b33755cbcbbe8cdce38ad8406
Dawidemm <dawid.mazur@icloud.com>
2024-04-15 11:54:34 +0200

    feat: Add saving egnerated images on train epoch end.

--- src/models/gan.py
+++ src/models/gan.py
@@ -14,7 +14,7 @@ class HandsGAN(lightning.LightningModule):
             latent_dim: int,
             generator: nn.Module,
             discriminator: nn.Module,
-            learning_rate: float=0.01
+            learning_rate: float=0.0005
     ):
         super().__init__()
 
@@ -24,6 +24,8 @@ class HandsGAN(lightning.LightningModule):
         self.generator = generator
         self.discriminator = discriminator
         self.learning_rate = learning_rate
+
+        self.epoch = -1
 
     def forward(self, z):
         return self.generator(z)
@@ -72,6 +74,9 @@ class HandsGAN(lightning.LightningModule):
         opt_discriminator.zero_grad()
         self.untoggle_optimizer(opt_discriminator)
 
+        self.g_loss = g_loss
+        self.d_loss = d_loss
+
     def configure_optimizers(self):
         opt_generator = torch.optim.Adam(self.generator.parameters(), lr=self.learning_rate)
         opt_discriminator = torch.optim.Adam(self.discriminator.parameters(), lr=self.learning_rate)
@@ -80,19 +85,21 @@ class HandsGAN(lightning.LightningModule):
     
     def on_train_epoch_end(self):
         
-        gen_img = self.generated_imgs[:8]
+        gen_img = self.generated_imgs[:4]
         gen_img = gen_img.to('cpu').detach().numpy()
-        fig, axs = plt.subplots(2, 4, figsize=(12, 6))
+        fig, axs = plt.subplots(2, 2, figsize=(6, 5))
 
         for i in range(2):
-            for j in range(4):
-                idx = i * 4 + j
+            for j in range(2):
+                idx = i * 2 + j
                 if idx < len(gen_img):
-                    img = gen_img[idx].reshape(256, 256, 3)
+                    img = gen_img[idx].reshape(128, 128, 3)
                     axs[i, j].imshow(img)
                     axs[i, j].axis('off')
                 else:
                     axs[i, j].axis('off')
 
+        self.epoch += 1
+
         plt.tight_layout()
-        plt.show()
+        plt.savefig(f'gan_imgs/epoch={self.epoch}-g_loss={self.g_loss}-d_loss={self.d_loss}.png')
