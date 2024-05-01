5ceb796b66d7f57f531f8793569170a7425bb892
Dawidemm <dawid.mazur@icloud.com>
2024-05-01 09:59:54 +0200

    feat: Remove normalization from transform.

--- src/utils/lit_hands_datamodule.py
+++ src/utils/lit_hands_datamodule.py
@@ -25,7 +25,6 @@ class LightningHandsDatamodule(LightningDataModule):
                 transforms.Resize((128, 128)),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ])
         else:
             self.transform = transform
